c9770e4cd5af268524fd1b639538eed47cca194c
Rusiru chapana <rusiru.chapana@atlato.com>
2026-09-01 13:14:41 +0530

    test45

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 async function loadBackend() {
-  const res = await fetch("http://localhost:8000/api/health");
+  const res = await fetch("/api/health");
   const data = await res.json();
   console.log(data);
 }
